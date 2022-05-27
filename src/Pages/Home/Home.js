1e886beaa79885e249b3d2f2dd48975cd8628871
Saklain Mustak <saklainmustak@sakmac.local>
2022-05-27 21:21:26 +0600

    firebase and tailwind added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Home page</h1>
+            <h1 className='text-amber-600'>Home page</h1>
+            <p className='bg bg-slate-900'>tailwind added</p>
         </div>
     );
 };
